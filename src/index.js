52e30cad6078d66f2a82c729dce0dbde7dd0b73b
Bali Tok <bali.tok@hotmail.com>
2023-04-03 15:03:02 +0800

    1.1 Exercise convert website to be done in react'

--- src/index.js
+++ src/index.js
@@ -6,9 +6,23 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.Fragment>
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Our Menu</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+    <div class="booking-button">
+      <button>Make a Booking</button>
+    </div>
+    <div class="btm-image">
+      <h2>Japanese so fresh, any fresh you have to fly to Japan</h2>
+    </div>
+  </React.Fragment>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
